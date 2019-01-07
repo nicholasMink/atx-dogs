f8227ecbaafba44e1b658f55e022e68e6c6a682a
nicholasMink <nick@enjoyresponsively.com>
2019-01-07 14:39:50 -0600

    ref: Add dog name, owner, address, description

--- src/components/Dogs/Dog/Dog.jsx
+++ src/components/Dogs/Dog/Dog.jsx
@@ -1,17 +1,46 @@
 import React, { Component } from 'react'
 
 export class Dog extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       dog: props.dog,
+    }
+  }
+
+  getOwnerName(first, last) {
+    return `${first} ${last}`
+  }
+
+  getDogName(str) {
+    let lastQuote = str.indexOf(',') || str.indexOf('"') || str.indexOf('”', 0);
+    let dogName = str.substring(1, lastQuote);
+    if (dogName.includes('"')) {
+      lastQuote = dogName.indexOf('"');
+      dogName = dogName.substring(0, lastQuote);
+    }
+    if (dogName.includes('”')) {
+      lastQuote = dogName.indexOf('”');
+      dogName = dogName.substring(0, lastQuote);
+    }
+    return dogName
+  }
+
   render() {
+    const {
+      dog,
+    } = this.state;
     return (
       <div className='Dog-wrapper'>
         <div className='Dog-header'>
-          <h4>Betsy</h4>
-          <p className='Dog-breed'>Pitbull</p>
+          <h4>{this.getDogName(dog.description_of_dog)}</h4>
+          <p className='Dog-breed'>{dog.zip_code}</p>
         </div>
         <div className='Dog-body'>
-        <p>Dog info</p>
-        <p>Owner info</p>
-        <p>Address</p>
+          <small>{dog.description_of_dog}</small>
+          <p>{this.getOwnerName(dog.first_name, dog.last_name)}</p>
+          <p>{`${dog.address}, ${dog.zip_code}`}</p>
         </div>
         <div className='Dog-footer'>
           <p className='Dog-action'>Dog Action</p>
--- src/components/Dogs/Dogs.jsx
+++ src/components/Dogs/Dogs.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Dog from './Dog/Dog'
 import './Dogs.scss'
 
 export class Dogs extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isLoading: true,
+       dogs: null,
+    }
+  }
+
+  getDogs() {
+    axios.get('https://data.austintexas.gov/resource/h8x4-nvyi.json')
+    .then(res => {
+      const dogs = res.data.map(dog => [dog]);
+      this.setState({dogs: dogs, isLoading: false})
+    })
+  }
+  
+  componentDidMount() {
+    this.setState({isLoading: true})
+    this.getDogs()
+  }
+
   render() {
+    const {
+      dogs,
+      isLoading,
+    } = this.state
+    if (isLoading) return <p>Loading</p>
     return (
-      <div>
-        <Dog />
+      <div className='Dogs-wrapper'>
+        {
+          dogs.map(dog => (dog) ? <Dog key={dog[0].description_of_dog} dog={dog[0]} /> : null)
+        }
       </div>
     )
   }
